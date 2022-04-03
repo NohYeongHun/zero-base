4d697788bffd6c58c8d93aff7ad4852a8172d7f7
YeongHun <dkzndk1499@gmail.com>
2022-04-04 07:07:04 +0900

    주석 추가

--- Java/JavaPackage/test/exam/Hello.java
+++ Java/JavaPackage/test/exam/Hello.java
@@ -1,7 +1,16 @@
 package JavaPackage.test.exam;
 
+/**
+ * @author 노영훈
+ */
 public class Hello{
+    /**
+     * static : 키워드
+     * String : 자료형
+     * helloWrold : 식별자
+     */
+    static String helloWorld = "Hello World!";
     public static void main(String[] args) {
-        System.out.println("package test");
+        System.out.println(helloWorld);
     }
 }
