cf818083d835d56ac2637e978ca5155a66dfca63
YeongHun <dkzndk1499@gmail.com>
2022-04-07 17:45:16 +0900

    switch 코드

---
+++ Java/JavaCondition/SwitchCode.java
@@ -0,0 +1,108 @@
+package JavaCondition;
+
+public class SwitchCode {
+    public static void main(String[] args) {
+        
+        int month = 1;
+        int day = 1;
+        
+        switch(day){
+            case 0:
+                System.out.printf("%d월 %d일 입니다.",month,day);
+                break;
+            case 1:
+                System.out.printf("%d월 %d일 입니다.",month,day);
+                break;
+            case 2:
+                System.out.printf("%d월 %d일 입니다.",month,day);
+                break;
+            case 3:
+                System.out.printf("%d월 %d일 입니다.",month,day);
+                break;
+            case 4:
+                System.out.printf("%d월 %d일 입니다.",month,day);
+                break;
+            case 5:
+                System.out.printf("%d월 %d일 입니다.",month,day);
+                break;
+            case 6:
+                System.out.printf("%d월 %d일 입니다.",month,day);
+                break;
+            case 7:
+                System.out.printf("%d월 %d일 입니다.",month,day);
+                break;
+            case 8:
+                System.out.printf("%d월 %d일 입니다.",month,day);
+                break;
+            case 9:
+                System.out.printf("%d월 %d일 입니다.",month,day);
+                break;
+            case 10:
+                System.out.printf("%d월 %d일 입니다.",month,day);
+                break;
+            case 11:
+                System.out.printf("%d월 %d일 입니다.",month,day);
+                break;
+            case 12:
+                System.out.printf("%d월 %d일 입니다.",month,day);
+                break;
+            case 13:
+                System.out.printf("%d월 %d일 입니다.",month,day);
+                break;
+            case 14:
+                System.out.printf("%d월 %d일 입니다.",month,day);
+                break;
+            case 15:
+                System.out.printf("%d월 %d일 입니다.",month,day);
+                break;
+            case 16:
+                System.out.printf("%d월 %d일 입니다.",month,day);
+                break;
+            case 17:
+                System.out.printf("%d월 %d일 입니다.",month,day);
+                break;
+            case 18:
+                System.out.printf("%d월 %d일 입니다.",month,day);
+                break;
+            case 19:
+                System.out.printf("%d월 %d일 입니다.",month,day);
+                break;
+            case 20:
+                System.out.printf("%d월 %d일 입니다.",month,day);
+                break;
+            case 21:
+                System.out.printf("%d월 %d일 입니다.",month,day);
+                break;
+            case 22:
+                System.out.printf("%d월 %d일 입니다.",month,day);
+                break;
+            case 23:
+                System.out.printf("%d월 %d일 입니다.",month,day);
+                break;
+            case 24:
+                System.out.printf("%d월 %d일 입니다.",month,day);
+                break;
+            case 25:
+                System.out.printf("%d월 %d일 입니다.",month,day);
+                break;
+            case 26:
+                System.out.printf("%d월 %d일 입니다.",month,day);
+                break;
+            case 27:
+                System.out.printf("%d월 %d일 입니다.",month,day);
+                break;
+            case 28:
+                System.out.printf("%d월 %d일 입니다.",month,day);
+                break;
+            case 29:
+                System.out.printf("%d월 %d일 입니다.",month,day);
+                break;
+            case 30:
+                System.out.printf("%d월 %d일 입니다.",month,day);
+                break;
+            case 31:
+                System.out.printf("%d월 %d일 입니다.",month,day);
+                break;
+        }
+    }
+}
